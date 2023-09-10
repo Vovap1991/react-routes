6c6c1d8e0069d1282a1c7564fda4f215fe89f9fc
Placebo98 <oleg.tsaryk98@gmail.com>
2023-09-10 16:53:35 +0300

    newChanges

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,12 +1,16 @@
 import { Section, Container, CountryInfo, Loader, Heading } from 'components';
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useLocation } from 'react-router-dom';
 import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
   const [data, setData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const location = useLocation();
+
+  const goBackLink = location?.state?.from ?? '/';
 
   const { countryId } = useParams();
   useEffect(() => {
@@ -25,7 +29,7 @@ export const Country = () => {
   }, [countryId]);
   return (
     <Section>
-      <Link to="/">Go home</Link>
+      <Link to={goBackLink}>Go back</Link>
       <Container>
         {loading && <Loader />}
         {error && <Heading>{error.messege}</Heading>}
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,41 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [country, setCountry] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const region = searchParams.get('query');
+    if (!region) return;
+    setLoading(true);
+    const findCoutry = async () => {
+      try {
+        const selectData = await fetchByRegion(region);
+        setCountry(selectData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    findCoutry();
+  }, [searchParams]);
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        {loading && <Loader />}
+        {error && <Heading>{error.message}</Heading>}
+        <SearchForm setSearchParams={setSearchParams} />
+        <CountryList countries={country} />
       </Container>
     </Section>
   );
